360e1bbf35424caf53b0793a4065304edc4a3d13
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-02-22 08:58:19 +0900

    Feat : front router or docs clear

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import MainLayout from "./layout";
+
+import { Provider, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const customization = useSelector((state) => state.customization);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider theme={customization}>
+        <Routes>
+          <Route path="/*" element={<MainLayout />}></Route>
+        </Routes>
+      </Provider>
+    </>
   );
 }
 
