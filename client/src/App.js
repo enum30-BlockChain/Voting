06feb89ce05960f0baa2e0a1967e2f4abbb515e4
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-02-22 09:51:05 +0900

    Feat : Candidate or Voter front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
-import MainLayout from "./layout";
-
-import { Provider, useSelector } from "react-redux";
+import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
+import MainLayout, { Candidate, Voter } from "./layout";
 
-function App() {
-  const customization = useSelector((state) => state.customization);
-  return (
-    <>
-      <Provider theme={customization}>
-        <Routes>
-          <Route path="/*" element={<MainLayout />}></Route>
-        </Routes>
-      </Provider>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="/Candidate" element={<Candidate />} />
+        <Route path="/Voter" element={<Voter />} />
+      </Routes>
+    );
+  }
 }
 
 export default App;
