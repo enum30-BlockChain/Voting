c6d229063fb8f466baead404e086040dcb3b26ed
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-02-23 16:28:47 +0900

    add router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
+import Candidate from "components/candidatesPage/Candidate";
+import MainLayout from "components/mainPage/Index";
+import Voter from "components/votePage/Voter";
 import React, { useEffect } from "react";
-import './App.css';
-import getWeb3 from './getWeb3';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import getWeb3 from "./getWeb3";
 
 function App() {
-  useEffect( async () => {
+  useEffect(async () => {
     const web3 = await getWeb3();
-    const accountList = await web3.eth.getAccounts(); 
+    const accountList = await web3.eth.getAccounts();
     console.log(accountList);
-    
-  }, [])
-  
+  }, []);
+
   return (
     <>
-      asdfadf
+      <Routes>
+        <Route path="/">
+          <Route index element={<MainLayout/>}></Route>
+          <Route path="/candidate" element={<Candidate/>} />
+          <Route path="/vote" element={<Voter/>} />
+          {/* <Route path="/votedone" element={<VoteDonePage/>} /> */}
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
