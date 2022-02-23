f0912a6588f4134f4132cbc1b17b56d35422440c
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-02-23 16:16:00 +0900

    add client folder

---
+++ client/src/components/candidatesPage/Candidate.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+// import addCandidateMethod from "../../votingContract/addCandidate.js";
+
+function Candidate() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const handleOnNameChange = (e) => {
+    setName(e.target.value);
+  }
+  const handleOnAgeChange = (e) => {
+    setAge(e.target.value);
+  }
+
+  const handleOnClick = () => {
+    // addCandidateMethod(name, age)
+  }
+
+  return (
+    <>
+      <div>후보자 페이지</div>
+      <div className="input-container">
+        <div className="input-name">
+          <h2>이름</h2>
+          <input type="text" onChange={handleOnNameChange}></input>
+        </div>
+      </div>
+      <div className="input-container">
+        <div className="input-name">
+          <h2>나이</h2>
+          <input type="number" onChange={handleOnAgeChange}></input>
+        </div>
+      </div>
+      <button onClick={handleOnClick}>등록</button>
+    </>
+  );
+}
+
+export default Candidate;
