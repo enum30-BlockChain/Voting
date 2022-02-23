891ed86f5d03faca5a719a87a8eddfa4964f0f0a
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-02-23 15:36:09 +0900

    clear all

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Root from "./Root";
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
-
-ReactDOM.render(<Root />, document.getElementById("root"));
-reportWebVitals();
