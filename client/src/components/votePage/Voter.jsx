1d5cdd22ded9c981ac22313b87643ab676cac18c
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-02-23 16:30:30 +0900

    Feat : voter page ing

--- client/src/components/votePage/Voter.jsx
+++ client/src/components/votePage/Voter.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 function Voter() {
   return (
     <>
       <div>투표자페이지</div>
+      <div className="input-container">
+        <div className="candidatename">후보자이름</div>
+        <input typeof="checkbox">투표를하겠다.</input>
+        <button>투표</button>
+      </div>
     </>
   );
 }
