896b517e51b0c810c414b4cb0d8ec4208a7b3463
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-02-24 20:39:09 +0900

    little

--- client/src/components/landingPage/navbar.jsx
+++ client/src/components/landingPage/navbar.jsx
@@ -4,8 +4,10 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
@@ -47,17 +49,6 @@ const ResponsiveAppBar = () => {
           </Link>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
