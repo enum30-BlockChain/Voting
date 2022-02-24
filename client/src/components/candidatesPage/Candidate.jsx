511d5aa414812c7aedb45569f7b0bd8465602766
ming <euphratesmin@gmail.com>
2022-02-24 11:47:32 +0900

    mui

--- client/src/components/candidatesPage/Candidate.jsx
+++ client/src/components/candidatesPage/Candidate.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button, Box } from "@material-ui/core";
 // import addCandidateMethod from "../../votingContract/addCandidate.js";
 
 function Candidate() {
@@ -42,41 +43,45 @@ function Candidate() {
 
   return (
     <>
-      <div>후보자 페이지</div>
-      <div className="input-container">
-        <div className="input-name">
-          <h2>이름</h2>
-          <input
-            type="text"
-            placeholder="이름"
-            maxlength="4"
-            value={name}
-            onChange={handleOnNameChange}
-          ></input>
+      <Box>
+        <div>후보자 페이지</div>
+        <div className="input-container">
+          <div className="input-name">
+            <h2>이름</h2>
+            <input
+              type="text"
+              placeholder="이름"
+              maxlength="4"
+              value={name}
+              onChange={handleOnNameChange}
+            ></input>
+          </div>
         </div>
-      </div>
-      <div className="input-container">
-        <div className="input-age">
-          <h2>나이</h2>
-          <input
-            type="text"
-            placeholder="나이"
-            value={age}
-            onChange={handleOnAgeChange}
-          ></input>
+        <div className="input-container">
+          <div className="input-age">
+            <h2>나이</h2>
+            <input
+              type="text"
+              placeholder="나이"
+              value={age}
+              onChange={handleOnAgeChange}
+            ></input>
+          </div>
         </div>
-      </div>
-      <button onClick={handleOnClick}>등록</button>
-      <h3>후보자리스트</h3>
-      <ul>
-        {candidate.map((a) => (
-          <box>
-            <div>후보자 번호 : {a.number}</div>
-            <div>후보자 이름 : {a.name}</div>
-            <div>후보자 나이 : {a.age}</div>
-          </box>
-        ))}
-      </ul>
+        <Button weight="30px" onClick={handleOnClick}>
+          등록
+        </Button>
+        <h3>후보자리스트</h3>
+        <ul>
+          {candidate.map((a) => (
+            <box>
+              <div>후보자 번호 : {a.number}</div>
+              <div>후보자 이름 : {a.name}</div>
+              <div>후보자 나이 : {a.age}</div>
+            </box>
+          ))}
+        </ul>
+      </Box>
     </>
   );
 }
