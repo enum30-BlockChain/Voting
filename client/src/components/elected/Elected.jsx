b604171b55879056b555de459385b600803ebe33
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-02-24 11:55:45 +0900

    Link add ob fix

---
+++ client/src/components/elected/Elected.jsx
@@ -0,0 +1,12 @@
+import Voter from "components/votePage/Voter";
+import React from "react";
+const Elected = () => {
+  return (
+    <>
+      <div>당선자페이지</div>
+      <Voter></Voter>
+    </>
+  );
+};
+
+export default Elected;
--- client/src/components/mainPage/Index.jsx
+++ client/src/components/mainPage/Index.jsx
@@ -4,6 +4,7 @@ import CandidateInfo from "./CandidateInfo";
 
 export { default as Candidate } from "../candidatesPage/Candidate";
 export { default as Voter } from "../votePage/Voter";
+export { default as Elected } from "../elected/Elected";
 
 const MainLayout = (e) => {
   const candidateList = [];
@@ -13,19 +14,24 @@ const MainLayout = (e) => {
 
   return (
     <>
+      <div className="menu">
+        <Link to="/candidate">
+          <button>candidate</button>
+        </Link>
+        <Link to="/vote">
+          <button>vote</button>
+        </Link>
+        <Link to="/elected">
+          <button>Eleted</button>
+        </Link>
+      </div>
+
       <div>메인페이지입니다.</div>
       <div className="candidateList-container">
         {candidateList.forEach((candidate) => (
           <CandidateInfo info={candidate} />
         ))}
       </div>
-
-      <Link to="/candidate">
-        <button>후보자등록페이지이동</button>
-      </Link>
-      <Link to="/vote">
-        <button>후보자등록페이지이동</button>
-      </Link>
     </>
   );
 };
